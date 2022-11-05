371fe481a1920d8807d48fcbf3ceca3ae31b0023
Jacob Sides <sides798@gmail.com>
2022-11-04 23:51:23 -0400

    Button layout

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -2,14 +2,15 @@
 	export let callback = () => {
 		window.alert('default callback');
 	};
-    export let color = 'grey';
-    const colorMap = {
-        grey: 'bg-gray-700',
-        red: 'bg-red-600 hover:bg-red-800',
-        green: 'bg-green-600 hover:bg-green-800'
-    }
+	export let color = 'grey';
+    export let _class; 
+	const colorMap = {
+		grey: 'bg-gray-700',
+		red: 'bg-red-600 hover:bg-red-800',
+		green: 'bg-green-600 hover:bg-green-800'
+	};
 </script>
 
-<button on:click={callback()} class={colorMap[color] + '  w-24 h-24 rounded-md p-4'}>
+<button on:click={callback()} class={colorMap[color] + '  rounded-md p-4 block ' + _class}>
 	<slot />
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
 	import { onMount } from 'svelte';
-	
+
 	import Button from '../components/button.svelte';
 	import StatusInput from '../components/statusInput.svelte';
-	
+
 	import { Log } from '$lib/Logger';
 	import { scrollPageToEndLeft } from './../lib/utils.js';
 
@@ -51,13 +51,14 @@
 		>
 			<h1 class="text-5xl lg:text-9xl opacity-75">What the fuck<br />are you doing?</h1>
 			<StatusInput _class="mt-20" {saveStatus} />
-			<div class="flex gap-8 mt-8">
-				<Button color="red">Not working</Button>
-				<Button color="green">working</Button>
-				<Button callback={deleteLast}>Delete last</Button>
+			<div class="grid gap-x-3 gap-y-5 mt-8 grid-cols-6">
+				<Button color="red" _class="col-span-3">Not working</Button>
+				<Button color="green" _class="col-span-3">working</Button>
+				<!-- <Button callback={deleteLast}>Delete last</Button> -->
+				<Button _class="sdcale-x-150 col-span-6">^</Button>
 			</div>
 		</div>
-		<div class="bg bg-black bg-opacity-40 w-full-h-full absolute inset-0 z-0" />
+		<div class="bg bg-black bg-opacity-40 w-full-h-full absolute inset-0 z-0 " />
 	</div>
 	<div
 		bind:this={statusRootElm}
@@ -66,7 +67,10 @@
 	>
 		<h2 class="sticky top-0 left-0 text-3xl">Entry for <br />{new Date().toDateString()}</h2>
 		{#each statuses as status}
-			<p>{status}</p>
+			<p class="max-w-[33%]">{status}</p>
 		{/each}
+	</div>
+	<div class="fixed bottom-12 right-12 z-10">
+		<Button callback={deleteLast} _class="hover:bg-opacity-75">Delete last</Button>
 	</div>
 </main>
